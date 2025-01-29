c0d1c3e2ff56740332128618596f0d0e405af366
nvanhoeck <niko.vanhoeck@gmail.com>
2025-01-29 09:23:36 +0100

    LRCA-0013: fixed the banishIfSuccumbed issue

--- utils/singASong.ts
+++ utils/singASong.ts
@@ -2,11 +2,13 @@ import type {Card} from "../model/Card";
 import {findOptimalSinger} from "./optimalSinger";
 import {transferElement} from "./transferElements";
 
-export const singASongCard = (hand: Card[], activeRow: Card[], cardIdx: number, inkTotal: number, banishedPile: Card[]) => {
+export const singASongCard = (hand: Card[], activeRow: Card[], cardIdx: number, inkTotal: number, banishedPile: Card[], optimalSingerIdx?: number) => {
     const card = hand[cardIdx]
-    const optimalSingerIdx = findOptimalSinger(activeRow, card.inkCost)
+    if (!optimalSingerIdx) {
+        optimalSingerIdx = findOptimalSinger(activeRow, card.inkCost)
+    }
     let newInkTotal = inkTotal - card.inkCost
-    if(optimalSingerIdx >= 0) {
+    if (optimalSingerIdx >= 0) {
         newInkTotal = inkTotal
         activeRow[optimalSingerIdx].readied = false
     }
