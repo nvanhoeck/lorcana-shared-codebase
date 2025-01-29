362e84e545a0c689bff7f307cd99f9f35518bb96
nvanhoeck <niko.vanhoeck@gmail.com>
2025-01-29 13:49:42 +0100

    LRCA-0016: eligible targets for bodyguards

--- utils/eligibleTargets.ts
+++ utils/eligibleTargets.ts
@@ -1,5 +1,8 @@
-import {Card} from "../model/Card";
+import {Card, isKeywordAbility} from "../model/Card";
 
 export const eligibleTargets = (opposingActiveRow: Card[]) => {
+    if(opposingActiveRow.find((c) => !c.readied && c.abilities.find((a) => isKeywordAbility(a) && a.keyword === 'Bodyguard'))) {
+        return opposingActiveRow.filter((c) => !c.readied && c.abilities.find((a) => isKeywordAbility(a) && a.keyword === 'Bodyguard'))
+    }
     return opposingActiveRow.filter((r) => !r.readied && r.type === 'Character' || r.type === 'Location');
 }
