4fbf8ca82b5e3fdf45b2353ec78840d8b451cf48
nvanhoeck <niko.vanhoeck@gmail.com>
2025-02-02 19:20:33 +0100

    LRCA-0023: implemented may-structure with modal. Need to do 004 additional ability and recheck other done abilities 'may' structure

--- model/abilities/bodyguard.ts
+++ model/abilities/bodyguard.ts
@@ -1,3 +1,14 @@
 import {type Card, isKeywordAbility} from "../Card";
+import {transferElement} from "../../utils";
+import type {Player} from "../Player";
 
 export const isBodyguard = (c: Card) => c.abilities.find((a) => isKeywordAbility(a) && a.keyword === 'Bodyguard')
+
+export const bodyguard = (player: Player, cardToBePlayedIdx: number) => {
+    const hand = player.hand
+    const activeRow = player.activeRow
+    hand[cardToBePlayedIdx].readied = false
+    hand[cardToBePlayedIdx].canBeReadiedDuringReadyPhase = true
+    transferElement(hand, activeRow, cardToBePlayedIdx)
+    return player
+}
