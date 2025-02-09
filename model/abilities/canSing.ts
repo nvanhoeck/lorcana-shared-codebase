02749b384edf83309f88ae58f15f8457929c41d6
nvanhoeck <niko.vanhoeck@gmail.com>
2025-02-09 17:33:37 +0100

    LRCA-0027: optimized sing condition

--- model/abilities/canSing.ts
+++ model/abilities/canSing.ts
@@ -4,7 +4,7 @@ const notVoiceless = (c: Card) => {
     return !c.abilities.find((a) => isStaticAbility(a) && a.name === 'VOICELESS')
 }
 
-export const canSing = (c: Card) => c.readied && c.type === 'Character' && notVoiceless(c);
+export const canSing = (c: Card, cost?: number) => c.readied && c.type === 'Character' && notVoiceless(c) && (cost ? singCostThreshold(c, cost): true );
 
 export const singCostThreshold = (c: Card, cost: number) => {
     const singerAbility = c.abilities.find((a) => isKeywordAbility(a) && a.keyword === 'Singer') as KeywordAbility
